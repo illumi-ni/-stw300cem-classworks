44a19f09da3948e7eed2bd1bca9355b547179034
“[Ujjal <[krazyme53@gmail.com]>
2020-12-17 17:59:09 +0545

    style/theme and scrollview

---
+++ app/src/main/java/com/ujjallamichhane/classworks/week2/StyleAndScrollViewActivity.kt
@@ -0,0 +1,12 @@
+package com.ujjallamichhane.classworks.week2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ujjallamichhane.classworks.R
+
+class StyleAndScrollViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style_and_scroll_view)
+    }
+}
