b578b58296d1292ad976eaab8e395c7b76516490
illumi-ni <krazyme53@gmail.com>
2020-12-17 22:13:32 +0545

    radio button activity

--- app/src/main/java/com/ujjallamichhane/classworks/week2/RadioButtonActivity.kt
+++ app/src/main/java/com/ujjallamichhane/classworks/week2/RadioButtonActivity.kt
@@ -1,37 +1,53 @@
-package com.ujjallamichhane.classworks
+package com.ujjallamichhane.classworks.week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.TextView
+import com.ujjallamichhane.classworks.R
 
-class MainActivity : AppCompatActivity() {
+class RadioButtonActivity : AppCompatActivity() {
     private lateinit var etFirst: EditText
     private lateinit var etSecond: EditText
+
+    private lateinit var radio: RadioGroup
+
     private lateinit var btnCalculate: Button
     private lateinit var tvResult: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_radio_button)
 
         etFirst = findViewById(R.id.etFirst)
         etSecond = findViewById(R.id.etSecond)
+        radio = findViewById(R.id.radio)
         btnCalculate = findViewById(R.id.btnCalculate)
         tvResult = findViewById(R.id.tvResult)
 
         btnCalculate.setOnClickListener {
             if (checkEmptyValues()) {
-                retrieveValues()
+                val id = radio.checkedRadioButtonId
+
+                val first: Int = etFirst.text.toString().toInt()
+                val second: Int = etSecond.text.toString().toInt()
+                var result: Int = 0
+                when (id) {
+                    R.id.rdoSum -> {
+                        result = first + second
+                    }
+                    R.id.rdoSubtract -> {
+                        result = first - second
+                    }
+                    R.id.rdoMultiply -> {
+                        result = first * second
+                    }
+                }
+                tvResult.text = "The result is: $result"
             }
         }
-    }
-
-    private fun retrieveValues() {
-        val first: Int = etFirst.text.toString().toInt()
-        val second: Int = etSecond.text.toString().toInt()
-        add(first, second)
     }
 
     private fun checkEmptyValues(): Boolean {
@@ -46,10 +62,5 @@ class MainActivity : AppCompatActivity() {
             flag = false
         }
         return flag
-    }
-
-    private fun add(first: Int, second: Int) {
-        val result = first + second
-        tvResult.text = result.toString()
     }
 }
